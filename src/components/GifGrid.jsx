d466f31862ac9cdfa5bdc9ad9359324ffd39a72d
IngridMH07 <ingrid.marenco@oktara.com>
2024-02-01 16:07:43 -0600

    added readme file

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -8,10 +8,7 @@ export const GifGrid = ( {category} ) => {
      
     const {images, isLoading} = useFetchGifs( category )
 
-    console.log(isLoading)
-
     return (
-        
         <>
         <div className ="card-grid"> 
             {isLoading ? <h1>Loading ...</h1> : 
